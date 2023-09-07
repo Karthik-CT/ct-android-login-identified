7acab67df7d6af194ed746a417570c998d22a832
Karthik Iyer <karthik.iyer@clevertap.com>
2023-09-07 12:24:21 +0530

    added push templates

--- app/src/main/java/com/karthik/ctlogin/MyFcmMessageListenerService.kt
+++ app/src/main/java/com/karthik/ctlogin/MyFcmMessageListenerService.kt
@@ -1,5 +1,8 @@
 package com.karthik.ctlogin
 
+import android.os.Bundle
+import android.util.Log
+import com.clevertap.android.sdk.CleverTapAPI
 import com.clevertap.android.sdk.pushnotification.fcm.CTFcmMessageHandler
 import com.google.firebase.messaging.RemoteMessage
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -7,7 +10,30 @@ import com.google.firebase.messaging.FirebaseMessagingService
 
 class MyFcmMessageListenerService : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
-        CTFcmMessageHandler()
-            .createNotification(applicationContext, message)
+//        CTFcmMessageHandler()
+//            .createNotification(applicationContext, message)
+
+        message.data.apply {
+            try {
+                if (isNotEmpty()) {
+                    val extras = Bundle()
+                    for ((key, value) in this) {
+                        extras.putString(key, value)
+                    }
+                    val info = CleverTapAPI.getNotificationInfo(extras)
+                    if (info.fromCleverTap) {
+//                        CleverTapAPI.createNotification(applicationContext, extras)
+                        CTFcmMessageHandler()
+                            .createNotification(applicationContext, message)
+                    } else {
+                        // not from CleverTap handle yourself or pass to another provider
+                    }
+                }
+            } catch (t: Throwable) {
+                Log.d("MYFCMLIST", "Error parsing FCM message", t)
+            }
+        }
+
+
     }
 }
